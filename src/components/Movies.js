715af79f109cf582d5117fa1ed35776253b75220
DenysVM <muratov.d@gmail.com>
2023-09-27 12:48:16 +0200

    1.1 MoviesPage.js fix

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,29 +1,11 @@
 // Movies.js
-import React, { useState } from 'react';
+import React from 'react';
+import MoviesPage from '../pages/MoviesPage';
 
 function Movies() {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-
-    const handleSearch = () => {
-        // Виконайте запит до /search/search-movies API зі словом пошуку і оновіть стан з отриманими фільмами
-    };
-
     return (
         <div>
-            <h2>Пошук фільмів</h2>
-            <input
-                type="text"
-                placeholder="Введіть ключове слово"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <button onClick={handleSearch}>Пошук</button>
-            <ul>
-                {searchResults.map((movie) => (
-                    <li key={movie.id}>{movie.title}</li>
-                ))}
-            </ul>
+            <MoviesPage />
         </div>
     );
 }
--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,23 +1,53 @@
-import React, { useState } from 'react';
+// MoviesPage.js
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { searchMovies } from '../components/MovieApi';
 
 function MoviesPage() {
+    const location = useLocation();
+    const navigate = useNavigate();
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
-    const handleSearch = () => {
-        // Виконайте запит до /search/search-movies API зі словом пошуку і оновіть стан з отриманими фільмами
+    useEffect(() => {
+        const query = new URLSearchParams(location.search).get('query') || '';
+        setSearchQuery(query);
+
+        if (query) {
+            async function fetchMovies() {
+                try {
+                    const movies = await searchMovies(query);
+                    setSearchResults(movies);
+                } catch (error) {
+                    console.error('Error searching movies:', error);
+                }
+            }
+
+            fetchMovies();
+        }
+    }, [location.search]);
+
+    const handleInputChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate(`?query=${searchQuery}`);
     };
 
     return (
         <div>
-            <h2>Пошук фільмів</h2>
-            <input
-                type="text"
-                placeholder="Введіть ключове слово"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <button onClick={handleSearch}>Пошук</button>
+            <h1>Search Movies</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={handleInputChange}
+                    placeholder="Search for movies"
+                />
+                <button type="submit">Search</button>
+            </form>
             <ul>
                 {searchResults.map((movie) => (
                     <li key={movie.id}>{movie.title}</li>
