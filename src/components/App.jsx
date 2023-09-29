c8f61aaf27554a419ba036f672fe40dd1e0bc56a
DenysVM <muratov.d@gmail.com>
2023-09-29 12:17:43 +0200

    2.1 layout fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,33 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom'; 
 import Navbar from './Navbar/Navbar';
+
+const Layout = ({ children }) => (
+  <div>
+    <Navbar />
+    {children}
+  </div>
+);
 
 const Home = lazy(() => import('./Home/Home'));
 const Movies = lazy(() => import('./Movies/Movies'));
 const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('./Cast/Cast')); 
-const Reviews = lazy(() => import('./Reviews/Reviews')); 
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} /> 
-            <Route path="reviews" element={<Reviews />} /> 
-          </Route>
-        </Routes>
-      </Suspense>
-    </Router>
-  );
-}
+const App = () => (
+  <Layout>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  </Layout>
+);
 
 export default App;
