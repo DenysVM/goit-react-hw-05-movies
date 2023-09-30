c1d606008ab7372b2f43e2adaa75854cfc61472f
DenysVM <muratov.d@gmail.com>
2023-09-30 13:14:07 +0200

    2.2.1 build-and-deploy fix

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { searchMovies } from '../../services/MovieApi';
 import styles from './Movies.module.css';
 import MoviesList from '../../components/MoviesList/MoviesList';
